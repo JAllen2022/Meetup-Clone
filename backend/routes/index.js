68b8f9b8ebab5cc621620acc069848f707915582
JAllen2022 <jasonallen715@gmail.com>
2022-12-14 13:37:24 -0800

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,14 +4,9 @@ const router = express.Router();
 
 // backend/routes/index.js
 // ...
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-  });
-  // ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 
 module.exports = router;
