6344bc6689d9f82fd4a77243b9a6bb7ff8cbbdc4
JAllen2022 <jasonallen715@gmail.com>
2022-12-15 15:20:35 -0800

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,5 +29,13 @@ router.post('/', async (req, res, next) => {
     }
   );
 
+// Log out
+// DELETE /api/session
+router.delete( '/', (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
 
 module.exports = router;
