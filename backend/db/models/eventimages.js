2b881f81e395063dca213696fb8e975ec39715b7
JAllen2022 <jasonallen715@gmail.com>
2022-12-18 20:50:09 -0800

    finished association for eventImages

--- backend/db/models/eventimages.js
+++ backend/db/models/eventimages.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      EventImage
+      EventImage.hasMany(models.Event,{foreignKey:'eventId'});
     }
   }
   EventImage.init({
