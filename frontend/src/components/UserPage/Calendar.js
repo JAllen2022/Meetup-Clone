bd15e7793089e7b90d23b340f74326b27b9ddd91
JAllen2022 <jasonallen715@gmail.com>
2023-01-27 18:48:20 -0800

    Calendar function now responds to click and changes dynamically

--- frontend/src/components/UserPage/Calendar.js
+++ frontend/src/components/UserPage/Calendar.js
@@ -1,30 +1,85 @@
+import { useEffect, useState } from "react";
 import "./Calendar.css";
 
 export default function Calendar() {
-    let tempArr=[]
-    for (let i = 1; i < 31; i++){
-        tempArr.push(<div className="days">{i}</div>);
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const date = new Date();
+  const year = date.getFullYear();
+  const [month, setMonth] = useState(date.getMonth());
+  const [monthString, setMonthString] = useState();
+  const [firstDay, setFirstDay] = useState();
+  const [lastDay, setLastDay] = useState();
+  const [displayYear, setDisplayYear] = useState(date.getFullYear());
+  const [previousLast, setPreviousLast] = useState();
+  // const [startWeekDay, setStartWeekDay] = useState();
+  const weekDay = firstDay?.getDay();
+
+  useEffect(() => {
+    setFirstDay(new Date(year, month, 1));
+    setLastDay(new Date(year, month + 1, 0));
+    setPreviousLast(new Date(year, month, 0));
+    setMonthString(monthNames[(new Date(year, month + 1, 0).getMonth())]);
+    setDisplayYear(new Date(year, month + 1, 0).getFullYear());
+  }, [month]);
+
+  console.log("checking date", displayYear);
+
+  const decreaseMonth = () => setMonth(month - 1);
+  const increaseMonth = () => setMonth(month + 1);
+
+  let tempArr = [];
+  const diffDays = previousLast ? previousLast.getDate() - weekDay : 0;
+  if (diffDays) {
+    for (let i = 1; i <= weekDay; i++) {
+      tempArr.push(<div className="days non-current">{diffDays + i}</div>);
     }
+  }
+  for (let i = 1; i <= lastDay?.getDate(); i++) {
+      tempArr.push(<div className="days">{i}</div>);
+  }
+  for (let i = 1; i < 7-lastDay?.getDay(); i++) {
+    tempArr.push(<div className="days non-current">{i}</div>);
+  }
+
+
   return (
     <div className="calendar-container">
       <div className="calendar-inner-container">
         <div className="calendar-title-container">
-          <span className="calendar-month-text">Month Year</span>
-          <span className="calendar-arrows cal-arr-left">
+          <span className="calendar-month-text">
+            {monthString} {displayYear}
+          </span>
+          <span
+            className="calendar-arrows cal-arr-left"
+            onClick={decreaseMonth}
+          >
             <i class="fa-solid fa-circle-chevron-left"></i>
           </span>
-          <span className="calendar-arrows">
+          <span className="calendar-arrows" onClick={increaseMonth}>
             <i class="fa-solid fa-circle-chevron-right"></i>
           </span>
         </div>
         <div className="calendar-day-container">
-          <div className="days">Su</div>
-          <div className="days">Mo</div>
-          <div className="days">Tu</div>
-          <div className="days">We</div>
-          <div className="days">Th</div>
-          <div className="days">Fr</div>
-          <div className="days">Sa</div>
+          <div className="weekdays">Su</div>
+          <div className="weekdays">Mo</div>
+          <div className="weekdays">Tu</div>
+          <div className="weekdays">We</div>
+          <div className="weekdays">Th</div>
+          <div className="weekdays">Fr</div>
+          <div className="weekdays">Sa</div>
           {tempArr}
         </div>
         <div className="calendar"></div>
