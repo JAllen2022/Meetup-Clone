95381056f83694e3856c79e8c71487c75601df3b
JAllen2022 <jasonallen715@gmail.com>
2023-04-28 10:50:04 -0700

    Fixed styling again on group page

--- frontend/src/components/EventPage/index.js
+++ frontend/src/components/EventPage/index.js
@@ -103,153 +103,155 @@ function EventPage() {
   useEffect(() => {
     dispatch(thunkGetSingleEvent(eventId));
     dispatch(thunkGetAttendees(eventId));
-    setAllowEdit(false)
+    setAllowEdit(false);
   }, [dispatch]);
   if (!event) return null;
 
   return (
     <div className="event-page-container">
-      <div className="event-page-header-container">
-        <div className="event-page-header-item-wrapper">
-          <h1 className="event-page-title">{event.name}</h1>
-          <div className="event-page-title-host-container">
-            <i className="fa-regular fa-circle-user full-rounded-profile"></i>
-            <div className="hosts-container">
-              <div>Hosted by</div>
-              <div>
-                {host ? `${host.firstName} ${host?.lastName}` : "Somebody"}
+      <div className="event-page-container-inner">
+        <div className="event-page-header-container">
+          <div className="event-page-header-item-wrapper">
+            <h1 className="event-page-title">{event.name}</h1>
+            <div className="event-page-title-host-container">
+              <i className="fa-regular fa-circle-user full-rounded-profile"></i>
+              <div className="hosts-container">
+                <div>Hosted by</div>
+                <div>
+                  {host ? `${host.firstName} ${host?.lastName}` : "Somebody"}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="event-body-container">
-        <div className="event-body">
-          <div className="event-body-left">
-            <div className="event-body-left-event-details">
-              <img
-                className="event-body-left-event-details-image"
-                src={
-                  eventImage
-                    ? eventImage.url
-                    : "https://secure.meetupstatic.com/next/images/fallbacks/group-cover-2-wide.webp"
-                }
-              />
+        <div className="event-body-container">
+          <div className="event-body">
+            <div className="event-body-left">
+              <div className="event-body-left-event-details">
+                <img
+                  className="event-body-left-event-details-image"
+                  src={
+                    eventImage
+                      ? eventImage.url
+                      : "https://secure.meetupstatic.com/next/images/fallbacks/group-cover-2-wide.webp"
+                  }
+                />
 
-              <h2>Details</h2>
-              <p>{event.description}</p>
-            </div>
-            <div>
-              <h3>Attendees ({event.numAttending})</h3>
-            </div>
-            <div className="event-attendees">
-              {attendees
-                ? attendeesArray.map((ele) => (
-                    <ProfileCard key={ele.id} member={ele} />
-                  ))
-                : null}
-            </div>
-          </div>
-          <div className="event-body-right">
-            <div className="event-body-right-sticky-container">
-              <div
-                onClick={directToGroup}
-                className="event-body-right-group-info-container"
-              >
-                <div className="event-body-right-group-info-image-container">
-                  <img
-                    className="event-body-right-group-info-image"
-                    src={
-                      groupInfo?.previewImage
-                        ? groupInfo.previewImage
-                        : "https://secure.meetupstatic.com/next/images/fallbacks/group-cover-2-wide.webp"
-                    }
-                  />
-                </div>
-                <div className="event-body-right-group-info">
-                  <div className="event-body-right-group-info-title">
-                    {groupInfo?.name}
-                  </div>
-                  <div className="event-body-righ-group-private-public">
-                    {groupInfo?.private ? "Private Group" : "Public Group"}
-                  </div>
-                </div>
+                <h2>Details</h2>
+                <p>{event.description}</p>
               </div>
-              <div className="event-body-right-event-info-container">
-                <div className="event-body-right-event-time-location-info">
-                  <div className="icon">
-                    <i className="fa-regular fa-clock fa-solid-event"></i>{" "}
-                  </div>
-                  <div className="event-body-right-event-info-right">
-                    <div> Begins: {startTimeString} </div>
-                    <div> Ends: {endTimeString} </div>
-                  </div>
-                </div>
-                <div className="event-body-right-event-time-location-info bottom-container-info">
-                  <div className="icon">
-                    <i className="fa-solid fa-location-dot fa-solid-event"></i>{" "}
-                  </div>
-                  <div className="event-body-right-event-info-right">
-                    {eventLocation}
-                  </div>
-                </div>
+              <div>
+                <h3>Attendees ({event.numAttending})</h3>
+              </div>
+              <div className="event-attendees">
+                {attendees
+                  ? attendeesArray.map((ele) => (
+                      <ProfileCard key={ele.id} member={ele} />
+                    ))
+                  : null}
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="event-sticky-footer">
-        <div className="event-sticky-footer-content">
-          <div className="event-sticky-footer-event-info-left">
-            <div className="event-sticky-footer-time-data">
-              {startTimeString}
-            </div>
-            <div className="event-sticky-footer-title-data">{event.name}</div>
-          </div>
-          <div className="event-sticky-footer-event-buttons-right">
-            {allowEdit && (
-              <>
+            <div className="event-body-right">
+              <div className="event-body-right-sticky-container">
                 <div
-                  ref={ulRef}
-                  className="event-sticky-footer-event-options"
-                  onClick={() => setShowMenu((prev) => !prev)}
+                  onClick={directToGroup}
+                  className="event-body-right-group-info-container"
                 >
-                  Event Actions <i className="fa-solid fa-angle-up"></i>
-                </div>
-                {showMenu && (
-                  <div className="event-sticky-footer-event-option-menu-container">
-                    <div className="event-sticky-footer-event-option-menu-inner-container">
-                      {/* {userType} */}
-                      <Link
-                        className="event-sticky-menu-link"
-                        to={`/events/${eventId}/edit`}
-                      >
-                        Edit Event
-                      </Link>
-                      <div>
-                        <OpenModalMenuItem
-                          itemText={
-                            <div className="event-sticky-menu-link">
-                              Delete Event
-                            </div>
-                          }
-                          onItemClick={closeMenu}
-                          modalComponent={
-                            <DeleteModal eventId={event.id} type={"Event"} />
-                          }
-                        />
-                      </div>
-                      {/* <Link className="event-sticky-menu-link">Add Image</Link> */}
+                  <div className="event-body-right-group-info-image-container">
+                    <img
+                      className="event-body-right-group-info-image"
+                      src={
+                        groupInfo?.previewImage
+                          ? groupInfo.previewImage
+                          : "https://secure.meetupstatic.com/next/images/fallbacks/group-cover-2-wide.webp"
+                      }
+                    />
+                  </div>
+                  <div className="event-body-right-group-info">
+                    <div className="event-body-right-group-info-title">
+                      {groupInfo?.name}
+                    </div>
+                    <div className="event-body-righ-group-private-public">
+                      {groupInfo?.private ? "Private Group" : "Public Group"}
                     </div>
                   </div>
-                )}
-              </>
-            )}
-            <div className="event-sticky-footer-attend-button">
-              <OpenModalMenuItem
-                itemText={<div className="">Attend</div>}
-                modalComponent={<FeatureComingSoon eventId={event.id} />}
-              />
+                </div>
+                <div className="event-body-right-event-info-container">
+                  <div className="event-body-right-event-time-location-info">
+                    <div className="icon">
+                      <i className="fa-regular fa-clock fa-solid-event"></i>{" "}
+                    </div>
+                    <div className="event-body-right-event-info-right">
+                      <div> Begins: {startTimeString} </div>
+                      <div> Ends: {endTimeString} </div>
+                    </div>
+                  </div>
+                  <div className="event-body-right-event-time-location-info bottom-container-info">
+                    <div className="icon">
+                      <i className="fa-solid fa-location-dot fa-solid-event"></i>{" "}
+                    </div>
+                    <div className="event-body-right-event-info-right">
+                      {eventLocation}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="event-sticky-footer">
+          <div className="event-sticky-footer-content">
+            <div className="event-sticky-footer-event-info-left">
+              <div className="event-sticky-footer-time-data">
+                {startTimeString}
+              </div>
+              <div className="event-sticky-footer-title-data">{event.name}</div>
+            </div>
+            <div className="event-sticky-footer-event-buttons-right">
+              {allowEdit && (
+                <>
+                  <div
+                    ref={ulRef}
+                    className="event-sticky-footer-event-options"
+                    onClick={() => setShowMenu((prev) => !prev)}
+                  >
+                    Event Actions <i className="fa-solid fa-angle-up"></i>
+                  </div>
+                  {showMenu && (
+                    <div className="event-sticky-footer-event-option-menu-container">
+                      <div className="event-sticky-footer-event-option-menu-inner-container">
+                        {/* {userType} */}
+                        <Link
+                          className="event-sticky-menu-link"
+                          to={`/events/${eventId}/edit`}
+                        >
+                          Edit Event
+                        </Link>
+                        <div>
+                          <OpenModalMenuItem
+                            itemText={
+                              <div className="event-sticky-menu-link">
+                                Delete Event
+                              </div>
+                            }
+                            onItemClick={closeMenu}
+                            modalComponent={
+                              <DeleteModal eventId={event.id} type={"Event"} />
+                            }
+                          />
+                        </div>
+                        {/* <Link className="event-sticky-menu-link">Add Image</Link> */}
+                      </div>
+                    </div>
+                  )}
+                </>
+              )}
+              <div className="event-sticky-footer-attend-button">
+                <OpenModalMenuItem
+                  itemText={<div className="">Attend</div>}
+                  modalComponent={<FeatureComingSoon eventId={event.id} />}
+                />
+              </div>
             </div>
           </div>
         </div>
