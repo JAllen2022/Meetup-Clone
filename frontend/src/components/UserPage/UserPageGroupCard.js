da62e9a1323924f1111f51ebf706a89bb38b7463
JAllen2022 <jasonallen715@gmail.com>
2023-05-03 22:26:41 -0700

    Clicking on groups on user page now route to group page

--- frontend/src/components/UserPage/UserPageGroupCard.js
+++ frontend/src/components/UserPage/UserPageGroupCard.js
@@ -1,17 +1,24 @@
-import './UserPageGroupCard.css'
+import { useHistory } from "react-router-dom";
+import "./UserPageGroupCard.css";
 
 export default function UserPageGroupCard({ group }) {
-    return (
-      <div className="user-page-group-card">
-        <div className="user-page-left-event-card-image-container">
-          <img
-            className="user-page-left-group-card-image"
-            src={group.previewImage ? group.previewImage : "https://secure.meetupstatic.com/next/images/home/Calendar2.svg?w=256"}
-          ></img>
-        </div>
-        <div className="user-page-left-event-card-title-text">
-          {group.name}
-        </div>
+  const history = useHistory();
+  return (
+    <div
+      className="user-page-group-card"
+      onClick={() => history.push(`/groups/${group.id}/about`)}
+    >
+      <div className="user-page-left-event-card-image-container">
+        <img
+          className="user-page-left-group-card-image"
+          src={
+            group.previewImage
+              ? group.previewImage
+              : "https://secure.meetupstatic.com/next/images/home/Calendar2.svg?w=256"
+          }
+        ></img>
       </div>
-    );
+      <div className="user-page-left-event-card-title-text">{group.name}</div>
+    </div>
+  );
 }
